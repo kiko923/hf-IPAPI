773e25ad6f7819aec0a611659365de8f86e681a2
kiko923 <75443014+kiko923@users.noreply.github.com>
2025-02-10 10:47:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,15 +6,17 @@ export default {
     // 处理 GET 请求中的 `ip` 参数
     const queryIP = url.searchParams.get("ip");
 
-    // 处理 POST 请求
+    // 处理 POST 请求（表单提交）
     if (request.method === "POST") {
       try {
-        const requestBody = await request.json();
-        if (requestBody.ip) {
-          userIP = requestBody.ip; // 使用 POST 传入的 IP
+        const formData = await request.formData();
+        const postIP = formData.get("ip");
+
+        if (postIP) {
+          userIP = postIP; // 使用 POST 传入的 IP
         }
       } catch (err) {
-        return new Response(JSON.stringify({ error: "Invalid JSON" }), {
+        return new Response(JSON.stringify({ error: "Invalid Form Data" }), {
           status: 400,
           headers: { "Content-Type": "application/json" },
         });
@@ -24,6 +26,14 @@ export default {
     // 如果 `?ip=` 存在，则优先使用它
     if (queryIP) {
       userIP = queryIP;
+    }
+
+    // IP 格式校验（仅允许 IPv4 或 IPv6）
+    if (!isValidIP(userIP)) {
+      return new Response(JSON.stringify({ error: "Invalid IP format" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
     }
 
     // 拼接 IP 到目标 API
@@ -51,3 +61,13 @@ export default {
     }
   }
 };
+
+// IP 地址格式校验函数
+function isValidIP(ip) {
+  const ipv4Regex =
+    /^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}$/;
+  const ipv6Regex =
+    /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/;
+
+  return ipv4Regex.test(ip) || ipv6Regex.test(ip);
+}
